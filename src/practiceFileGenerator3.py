b1497ac61db0c2d92036e1f169e02dbbe130b1ea
kevin <kevinthomassssquigley@gmail.com>
2021-08-31 21:43:47 +0000

    Improvements to practice generators

---
+++ src/practiceFileGenerator3.py
@@ -0,0 +1,12 @@
+import datetime as dt
+import pandas as pd
+
+weatherHead = pd.read_csv('hly175.csv',skiprows = (0,1,2,3,4,5,6,7,8,9,10,11,12,13))
+weatherHead['date2'] = [dt.datetime.strptime(d,"%d-%b-%Y %H:%M") for d in weatherHead['date'] ]
+weatherHead['date_for_merge'] = weatherHead['date2'].dt.round('H')
+weatherHead = weatherHead[(weatherHead['date2'] >= '2020-04-01') & (weatherHead['date2'] < '2020-07-01')]
+weatherHead =weatherHead.drop(['date_for_merge','date2'], axis=1)
+print(weatherHead.head(2000).to_csv("hly175-head.csv", index=False))
+weatherHead = pd.read_csv('2020Q2.csv')
+print(weatherHead.head(20000).to_csv('2020Q2-head.csv', index=False))
+
